a0127b52f8155c59491060e0934e16925525ddb8
ekundee <isaiahekundayo17@gmail.com>
2022-01-24 13:40:28 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const express  = require("express")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+const path = require("path")
+const ejs = require("ejs")
+const req = require("express/lib/request")
+
+
+var app = express()
+
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname + "./views"))
+app.set("layout", path.join(__dirname + "./views/layouts"))
+app.set("partials", path.join(__dirname + "./views/partials"))
+
+
+app.get("/",(req,res)=>{
+    res.send("Speed coding")
+})
+
+
+port = process.env.PORT || 3000
+app.listen(port)
