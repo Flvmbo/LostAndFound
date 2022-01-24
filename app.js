098fb730e059b04cd1b4a586233f129670b4f97e
ekundee <isaiahekundayo17@gmail.com>
2022-01-24 15:38:20 +0100

    connecting the frontend pages to ejs templating engine
    node backend integration

--- app.js
+++ app.js
@@ -7,15 +7,17 @@ const req = require("express/lib/request")
 
 
 var app = express()
+app.use(express.static("./public"))
+
 
 app.set("view engine", "ejs")
-app.set("views", path.join(__dirname + "./views"))
-app.set("layout", path.join(__dirname + "./views/layouts"))
-app.set("partials", path.join(__dirname + "./views/partials"))
+app.set("views", path.join(__dirname + "/views"))
+app.set("layout", path.join(__dirname + "/views/layouts"))
+app.set("partials", path.join(__dirname + "/views/partials"))
 
 
 app.get("/",(req,res)=>{
-    res.send("Speed coding")
+    res.render("item_retrieval_page")
 })
 
 
