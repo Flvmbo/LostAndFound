f86477d581e5abe78c58a36611a28158128dedb0
Todi <todimubamtefa11@gmail.com>
2022-03-04 17:01:11 +0100

    added lil animation to landing page

--- public/landing_page_files/action.js
+++ public/landing_page_files/action.js
@@ -1,31 +1,31 @@
-var count = 1
-var margin = 0
-var mP = 0
-function changeSlide()
-{
+// var count = 1
+// var margin = 0
+// var mP = 0
+// function changeSlide()
+// {
     
-    var slide = document.getElementsByClassName("current-item")[0]
-    slide.style.marginLeft = `calc(${margin}rem - ${mP}%) `
-    margin-=42
-    mP +=5
-    count++
-    if(count > 3)
-    {
-        count = 1
-        margin = 0
-        mP=0
-    }
-}
+//     var slide = document.getElementsByClassName("current-item")[0]
+//     slide.style.marginLeft = `calc(${margin}rem - ${mP}%) `
+//     margin-=42
+//     mP +=5
+//     count++
+//     if(count > 3)
+//     {
+//         count = 1
+//         margin = 0
+//         mP=0
+//     }
+// }
 
-setInterval(changeSlide,3000)
+// setInterval(changeSlide,3000)
 
 
 
-var aboutUsHeader = document.querySelectorAll('.abs')
-let aboutUsText = document.querySelectorAll('.yes')
-let adminPic = document.querySelector('.f-square2')
+var aboutUsHeader = document.querySelectorAll('.about-header')
+var aboutText = document.querySelectorAll('.about-desc')
+let adminSection = document.querySelector('.admin-section-info')
 let hhsAdmin = document.querySelector('.hhs-admin')
-let adminYarns = document.querySelector('.admin-yarns')
+let adminYarns = document.querySelector('.admin-text-container')
 let arBtn = document.querySelector('.ar-btn')
 
 var options = {
@@ -48,63 +48,54 @@ const inViewCallback = entries => {
   let observer = new IntersectionObserver(inViewCallback,options); 
 
   observer.observe(aboutUsHeader[0])
-  observer.observe(aboutUsHeader[1])
+  aboutText.forEach(element => {
+
+      observer.observe(element)
+  })
+  observer.observe(adminSection)
   observer.observe(adminPic)
   observer.observe(hhsAdmin)
   observer.observe(arBtn)
-  observer.observe(adminYarns)
-  aboutUsText.forEach(element => {
-      observer.observe(element)
-  })
+//   observer.observe(adminYarns)
 
 
 
 
-// const newCursor = document.querySelector('.updatedCursor')
-// document.addEventListener('mousemove',function(e)
+// const hamburger = document.querySelector('.hamburger')
+// const mobileMenu = document.querySelector('.mobile-menu-container')
+
+// //
+// const dividingLine =  document.getElementById('dividingline')
+// const mobileLeft =  document.getElementById('mobileLeft')
+// const mobileRight =  document.getElementById('mobileRight')
+
+// const hamburgerCloseDiv = document.getElementById("hamburgerCloseDiv")
+// const closee = document.getElementById("close")
+
+// hamburger.addEventListener('click',function(e)
 // {
-//     var x = e.pageX
-//     var y = e.pageY
+//     hamburgerCloseDiv.style.display = "block"
+//     mobileMenu.classList.toggle('mobile-opened')
+//     hamburger.style.position = "absolute"
 
-//     newCursor.style.top = y + "px"
-//     newCursor.style.left = x + "px"
+
+//     if(document.body.style.overflowY == 'hidden')
+//     {   document.body.style.overflowY = 'auto' }
+//     else
+//     {
+//         document.body.style.overflowY = 'hidden'
+//     }
 // })
 
-const hamburger = document.querySelector('.hamburger')
-const mobileMenu = document.querySelector('.mobile-menu-container')
 
-//
-const dividingLine =  document.getElementById('dividingline')
-const mobileLeft =  document.getElementById('mobileLeft')
-const mobileRight =  document.getElementById('mobileRight')
-
-const hamburgerCloseDiv = document.getElementById("hamburgerCloseDiv")
-const closee = document.getElementById("close")
-
-hamburger.addEventListener('click',function(e)
-{
-    hamburgerCloseDiv.style.display = "block"
-    mobileMenu.classList.toggle('mobile-opened')
-    hamburger.style.position = "absolute"
-
-
-    if(document.body.style.overflowY == 'hidden')
-    {   document.body.style.overflowY = 'auto' }
-    else
-    {
-        document.body.style.overflowY = 'hidden'
-    }
-})
-
-
-closee.addEventListener("click",()=>{
-    hamburgerCloseDiv.style.display = "none"
-    mobileMenu.classList.toggle('mobile-opened')
-    hamburger.style.position = "relative"
-    if(document.body.style.overflowY == 'hidden')
-    {   document.body.style.overflowY = 'auto' }
-    else
-    {
-        document.body.style.overflowY = 'hidden'
-    }
-})
+// closee.addEventListener("click",()=>{
+//     hamburgerCloseDiv.style.display = "none"
+//     mobileMenu.classList.toggle('mobile-opened')
+//     hamburger.style.position = "relative"
+//     if(document.body.style.overflowY == 'hidden')
+//     {   document.body.style.overflowY = 'auto' }
+//     else
+//     {
+//         document.body.style.overflowY = 'hidden'
+//     }
+// })
