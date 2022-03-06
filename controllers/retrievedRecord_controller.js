d5c6e188ec11373b65b34bed61f649e70264fee0
ekundee <isaiahekundayo17@gmail.com>
2022-03-06 19:16:03 +0100

    sessions_completion

--- controllers/retrievedRecord_controller.js
+++ controllers/retrievedRecord_controller.js
@@ -3,7 +3,7 @@ const retrieved_items = require("../model/retrieve_Item")
 const get_showRetrievedRecordPage = async (req,res)=>{
     try{
         var retrieved_items_array = await retrieved_items.find({})
-        res.render("item_retrieval_record_page", {info:{items:retrieved_items_array}})
+        res.render("item_retrieval_record_page", {info:{items:retrieved_items_array, activeAdmin : req.session.activeAdmin}})
     }catch(err){
         console.log(err)
     }
