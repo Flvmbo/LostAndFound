f5e98095a839cf7750ed259c84fd0a5f76e51e9b
ekundee <isaiahekundayo17@gmail.com>
2022-03-06 20:28:20 +0100

    solve reload problem for submit item page

--- controllers/item_controller.js
+++ controllers/item_controller.js
@@ -21,63 +21,53 @@ const post_submitItem =  async (req,res)=>{
     var uniqueID =  Date.now()
 
     const {item_name,Category,Description,Location} = req.body
-    if(item_name == "" || Description == "" || Location == "" || Category=="N/A"){
-    
-        res.render("found_an_item", {info:{error:"Enter values to empty fields", display:"block", modalDisplay:"none", activeAdmin: req.session.activeAdmin}});
-    }
-    else{
-        if(req.files.length == 2){
-            try{
-                var new_item =  new items({
-                    uniqueID : uniqueID,
-                    item_name : item_name,
-                    Category : Category,
-                    Description : Description,
-                    Location : Location,
-                    itemDate:  dateTime,
-                    firstImage:  "uploads/images/" + req.files[0].filename, 
-                    secondImage: "uploads/images/" + req.files[1].filename
-                })
-                
-                await new_item.save().then( async ()=>{
 
-                    res.render("found_an_item",{info:{popup:"show"}})
-                    // res.redirect('/lost-items');
-                    var get_update_list = await get_update_schema.find({Category : Category});
-                    for (let i = 0; i < get_update_list.length; i++) {
-                        console.log(get_update_list[i].id)
-                        var transporter = nodemailer.createTransport({
-                            service: 'gmail',
-                            auth: {
-                                user: 'isaiah.ekundayo@stu.cu.edu.ng',
-                                pass: 'Oluwadamilare3#'
-                            }
-                        });
-                        var mailOptions = {
-                            from: 'isaiah.ekundayo@stu.cu.edu.ng',
-                            to: get_update_list[i].StudentEmail,
-                            subject: 'Lost Item Alert',
-                            html: `<p>Based on your the item misplaced you have been adviced to check out this <a href="http://localhost:3700/check-item/${uniqueID}">item</a></p>
+
+    var new_item =  new items({
+        uniqueID : uniqueID,
+        item_name : item_name,
+        Category : Category,
+        Description : Description,
+        Location : Location,
+        itemDate:  dateTime,
+        firstImage:  "uploads/images/" + req.files[0].filename, 
+        secondImage: "uploads/images/" + req.files[1].filename
+    })
+                
+    await new_item.save().then( async ()=>{
+
+    res.render("found_an_item",{info:{popup:"show"}})
+    // res.redirect('/lost-items');
+    var get_update_list = await get_update_schema.find({Category : Category});
+    for (let i = 0; i < get_update_list.length; i++) {
+        console.log(get_update_list[i].id)
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+            user: 'isaiah.ekundayo@stu.cu.edu.ng',
+            pass: 'Oluwadamilare3#'
+            }
+        });
+    
+        var mailOptions = {
+            from: 'isaiah.ekundayo@stu.cu.edu.ng',
+            to: get_update_list[i].StudentEmail,
+            subject: 'Lost Item Alert',
+            html: `<p>Based on your the item misplaced you have been adviced to check out this <a href="http://localhost:3700/check-item/${uniqueID}">item</a></p>
                             <p>To opt out from receiving emails please click the button <a href = "http://localhost:3700/unsubscribe"><input type="button"/ value="Unsubscribe"></a> </p>
                             `
-                        };
-                        transporter.sendMail(mailOptions, function(error, info){
-                            if (error) {
-                                console.log(error);
-                            } else {
-                                console.log('Email sent: ' + info.response);
-                            }
-                        }); 
-                    }          
-                })
-            }catch(e){
-                console.log(e.message);
+        };
+    
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
             }
-        }
-        else{
-            res.render("found_an_item", {info:{error:"Both images are required", display:"block", modalDisplay:"none", activeAdmin: req.session.activeAdmin}});
-        }
-    }
+        }); 
+    }          
+    })
+   
   
 }
 
