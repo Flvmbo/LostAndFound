979b25c6a2c98f513ddf460318d4863c703b3d46
Todi <todimubamtefa11@gmail.com>
2022-03-06 13:42:41 +0100

    updated landing page

--- public/landing_page_files/action.js
+++ public/landing_page_files/action.js
@@ -1,25 +1,3 @@
-// var count = 1
-// var margin = 0
-// var mP = 0
-// function changeSlide()
-// {
-    
-//     var slide = document.getElementsByClassName("current-item")[0]
-//     slide.style.marginLeft = `calc(${margin}rem - ${mP}%) `
-//     margin-=42
-//     mP +=5
-//     count++
-//     if(count > 3)
-//     {
-//         count = 1
-//         margin = 0
-//         mP=0
-//     }
-// }
-
-// setInterval(changeSlide,3000)
-
-
 
 var aboutUsHeader = document.querySelectorAll('.about-header')
 var aboutText = document.querySelectorAll('.about-desc')
@@ -53,49 +31,7 @@ const inViewCallback = entries => {
       observer.observe(element)
   })
   observer.observe(adminSection)
-  observer.observe(adminPic)
-  observer.observe(hhsAdmin)
-  observer.observe(arBtn)
+  // observer.observe(adminPic)
+  // observer.observe(hhsAdmin)
+  // observer.observe(arBtn)
 //   observer.observe(adminYarns)
-
-
-
-
-// const hamburger = document.querySelector('.hamburger')
-// const mobileMenu = document.querySelector('.mobile-menu-container')
-
-// //
-// const dividingLine =  document.getElementById('dividingline')
-// const mobileLeft =  document.getElementById('mobileLeft')
-// const mobileRight =  document.getElementById('mobileRight')
-
-// const hamburgerCloseDiv = document.getElementById("hamburgerCloseDiv")
-// const closee = document.getElementById("close")
-
-// hamburger.addEventListener('click',function(e)
-// {
-//     hamburgerCloseDiv.style.display = "block"
-//     mobileMenu.classList.toggle('mobile-opened')
-//     hamburger.style.position = "absolute"
-
-
-//     if(document.body.style.overflowY == 'hidden')
-//     {   document.body.style.overflowY = 'auto' }
-//     else
-//     {
-//         document.body.style.overflowY = 'hidden'
-//     }
-// })
-
-
-// closee.addEventListener("click",()=>{
-//     hamburgerCloseDiv.style.display = "none"
-//     mobileMenu.classList.toggle('mobile-opened')
-//     hamburger.style.position = "relative"
-//     if(document.body.style.overflowY == 'hidden')
-//     {   document.body.style.overflowY = 'auto' }
-//     else
-//     {
-//         document.body.style.overflowY = 'hidden'
-//     }
-// })
