6484af042a77f1221e07d741f7a929e92c691729
Callmedoziee <francisnwosu017@gmail.com>
2022-03-18 20:44:40 +0100

    records page optimization

--- controllers/retrieveItem_controller.js
+++ controllers/retrieveItem_controller.js
@@ -1,49 +1,53 @@
 const retrievedItems = require('../model/retrieve_Item');
 const lostitems = require('../model/submit_item');
 
-const get_showRetrieveItemPage = async (req, res) => {
+const get_showRetrieveItemPage = async(req, res) => {
     id = req.params.id;
     // console.log(id)
-    res.render("item_retrieval_page", {info:{modalDisplay:"none", id:`${id}`, activeAdmin: req.session.activeAdmin}});
+    res.render("item_retrieval_page", { info: { modalDisplay: "none", id: `${id}`, activeAdmin: req.session.activeAdmin } });
 }
 
 
-const post_retriveItem =  async (req,res)=>{
-    try{
+const post_retriveItem = async(req, res) => {
+    try {
         let getId = req.params.id;
         console.log(getId)
-        const {firstname, lastname, admin, location} = req.body;
-                var retrieved_item = await lostitems.findOne({id : getId})
-                const date = new Date()
+        const { firstname, lastname, admin, location } = req.body;
+        var retrieved_item = await lostitems.findOne({ id: getId });
+        var today = new Date();
+        var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        var dateTime = date + " " + time;
 
-                var retrievedItem =   new retrievedItems({
-                    firstname:firstname,
-                    lastname:lastname,
-                    admin:admin,
-                    location:location,
-                    item_name : retrieved_item.item_name,
-                    Location_found : retrieved_item.Location,
-                    date_found : date.getDate()+"-"+date.getDay()+"-"+date.getFullYear()+"  "+date.getHours()+":"+date.getMinutes(),                
-                    idphoto:"retrieved/images/" + req.file.filename,
-                    itemPhoto : retrieved_item.firstImage
+        var retrievedItem = new retrievedItems({
+            firstname: firstname,
+            lastname: lastname,
+            admin: admin,
+            location: location,
+            item_name: retrieved_item.item_name,
+            Location_found: retrieved_item.Location,
+            date_retrieved: dateTime,
+            date_found: retrieved_item.itemDate,
+            idphoto: "retrieved/images/" + req.file.filename,
+            itemPhoto: retrieved_item.firstImage,
+            category: retrieved_item.Category
+        })
 
-                })
-                
-            await retrievedItem.save()
-            .then(()=> res.render("item_retrieval_page", {info:{modalDisplay:"block",id:`${id}`, activeAdmin: req.session.activeAdmin, popup: "show"}}))
-            .catch((e)=>console.log(e.message));
-            
-            await lostitems.deleteOne({_id:getId}).catch((e)=>console.log(e.message))
+        await retrievedItem.save()
+            .then(() => res.render("item_retrieval_page", { info: { modalDisplay: "block", id: `${id}`, activeAdmin: req.session.activeAdmin, popup: "show" } }))
+            .catch((e) => console.log(e.message));
 
-    }catch(e){
+        await lostitems.deleteOne({ _id: getId }).catch((e) => console.log(e.message))
+
+    } catch (e) {
         console.log(e);
-        res.render("item_retrieval_page", {info:{error:"Please reload application", display:"block", modalDisplay:"none", activeAdmin: req.session.activeAdmin}});
+        res.render("item_retrieval_page", { info: { error: "Please reload application", display: "block", modalDisplay: "none", activeAdmin: req.session.activeAdmin } });
     }
 }
 
 
 
-module.exports ={
+module.exports = {
     post_retriveItem,
     get_showRetrieveItemPage
 }
--- model/retrieve_Item.js
+++ model/retrieve_Item.js
@@ -1,43 +1,49 @@
 const mongoose = require('mongoose');
 
 var retrieveItem = new mongoose.Schema({
-    firstname:{
-        required:true,
-        type:String
+    firstname: {
+        required: true,
+        type: String
     },
-    lastname:{
-        required:true,
-        type:String
+    lastname: {
+        required: true,
+        type: String
     },
-    admin:{
-        required:true,
-        type:String
+    admin: {
+        required: true,
+        type: String
     },
-    location:{
-        required:true,
-        type:String
+    location: {
+        required: true,
+        type: String
     },
-    item_name : {
-        required:true,
-        type:String
+    item_name: {
+        required: true,
+        type: String
     },
-    Location_found :{
-        required:true,
-        type:String
+    Location_found: {
+        required: true,
+        type: String
     },
-    date_found :{
-        required:true,
-        type:String
+    date_found: {
+        required: true,
+        type: String
     },
-                
-    idphoto:{
-        type:String
+    date_retrieved: {
+        type: String
     },
 
-    itemPhoto:{
-        type:String
+    idphoto: {
+        type: String
+    },
+
+    itemPhoto: {
+        type: String
+    },
+    category: {
+        type: String
     }
 })
 
-var retrievedRecordSchema = mongoose.model("Retrieved Record",retrieveItem)
+var retrievedRecordSchema = mongoose.model("Retrieved Record", retrieveItem)
 module.exports = retrievedRecordSchema
