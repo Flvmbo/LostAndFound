af39786c2c130da331c72706c908b7dd4f3b3515
Callmedoziee <francisnwosu017@gmail.com>
2022-03-18 16:05:34 +0100

    retrieval records updtate

--- controllers/adminLogin_controller.js
+++ controllers/adminLogin_controller.js
@@ -1,59 +1,57 @@
 const items = require('../model/submit_item')
 const admin = require('../model/users')
 
-const get_adminLoginPage = async (req,res) =>{
-    res.render("admin_signin_page", {info:{error:"", display:"none"}});
+const get_adminLoginPage = async(req, res) => {
+    res.render("admin_signin_page", { info: { error: "", display: "none" } });
 }
 
-const get_adminLoginPageFromRetrieveItem = async (req,res) =>{
+const get_adminLoginPageFromRetrieveItem = async(req, res) => {
     id = req.params.id
-    res.render("admin_signin_page", {info:{error:"", display:"none", id : id}});
+    res.render("admin_signin_page", { info: { error: "", display: "none", id: id } });
 }
 
-const post_signIn = async (req,res) =>{
-    try{
+const post_signIn = async(req, res) => {
+    try {
         //getting the values of Username and password from req.body
-        const {Username , Password} = req.body
-        //Username and password authentication
-            const getadmin =  await admin.findOne({username:Username, password:Password});
-            if(getadmin != null){     
-                // store the admin's location as a session variable
-                try{
-                    req.session.activeAdmin = getadmin.location
-                    res.redirect("/lost-items")
+        const { Username, Password } = req.body
+            //Username and password authentication
+        const getadmin = await admin.findOne({ username: Username, password: Password });
+        if (getadmin != null) {
+            // store the admin's location as a session variable
+            try {
+                req.session.activeAdmin = getadmin.location
+                res.redirect("/lost-items")
                     // const allItems = await items.find({})
                     // res.render("lost_items_page",{items:{item:allItems, activeAdmin: req.session.activeAdmin}})
-                }catch(error)
-                {
-                    console.log(error);
-                }
-            }else{
-                res.render("admin_signin_page", {info:{error:"Invalid Username or password", display:"block" }});
+            } catch (error) {
+                console.log(error);
             }
-    }catch(error){
+        } else {
+            res.render("admin_signin_page", { info: { error: "Invalid Username or password", display: "block" } });
+        }
+    } catch (error) {
         console.log(error);
     }
 }
 
-const post_signinFromRetrieveItem = async (req,res) => {
-    try{
+const post_signinFromRetrieveItem = async(req, res) => {
+    try {
         //getting the values of Username and password from req.body
-        const {Username , Password} = req.body
-            const getadmin =  await admin.findOne({username:Username, password:Password});
-            if(getadmin != null){    
-                // store the admin's location as a session variable
-                try{
-                    //store the admin's location as a session variable
-                    req.session.activeAdmin = getadmin.location
-                    res.redirect(`/item-retrieval/${req.params.id}`)
-                }catch(error)
-                {
-                    console.log(error);
-                }
-            }else{
-                res.render("admin_signin_page", {info:{error:"Invalid Username or password", display:"block", }});
+        const { Username, Password } = req.body
+        const getadmin = await admin.findOne({ username: Username, password: Password });
+        if (getadmin != null) {
+            // store the admin's location as a session variable
+            try {
+                //store the admin's location as a session variable
+                req.session.activeAdmin = getadmin.location
+                res.redirect(`/item-retrieval/${req.params.id}`)
+            } catch (error) {
+                console.log(error);
             }
-    }catch(e){
+        } else {
+            res.render("admin_signin_page", { info: { error: "Invalid Username or password", display: "block", } });
+        }
+    } catch (e) {
         console.log(e);
     }
 }
--- controllers/retrievedRecord_controller.js
+++ controllers/retrievedRecord_controller.js
@@ -1,10 +1,11 @@
 const retrieved_items = require("../model/retrieve_Item")
 
-const get_showRetrievedRecordPage = async (req,res)=>{
-    try{
-        var retrieved_items_array = await retrieved_items.find({})
-        res.render("item_retrieval_record_page", {info:{items:retrieved_items_array, activeAdmin : req.session.activeAdmin}})
-    }catch(err){
+const get_showRetrievedRecordPage = async(req, res) => {
+    try {
+        var retrieved_items_array = await retrieved_items.find({}).sort({ itemDate: -1 })
+        const number = retrieved_items_array.length;
+        res.render("item_retrieval_record_page", { info: { items: retrieved_items_array, activeAdmin: req.session.activeAdmin, num: number } })
+    } catch (err) {
         console.log(err)
     }
 }
