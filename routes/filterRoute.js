7d1b34c42f6e78438b60a1e1c7aca62b07723e42
ekundee <isaiahekundayo17@gmail.com>
2022-03-21 11:42:11 +0100

    Filter something

--- routes/filterRoute.js
+++ routes/filterRoute.js
@@ -4,14 +4,25 @@ const lostItemsModel = require('../model/submit_item');
 route.post("/filter", async (req, res)=>{
     console.log("received", req.body);
     let {category, location} = req.body;
-    console.log("category is: ", category);
+    //console.log("category is: ", category);
     req.session.sessionCategory = category;
     req.session.sessionLocation = location;
+    console.log("category is: " + location);
+
     try{
-        var lostItems = await lostItemsModel.find({}).sort( {itemDate: -1 })   
+        if(req.session.sessionCategory == "" && req.session.sessionLocation == ""){
+            var lostItems = await lostItemsModel.find({}).sort( {itemDate: -1 })   
+        }else if(req.session.sessionCategory == ""){
+            var lostItems = await lostItemsModel.find({Location : {$all : req.session.sessionLocation}}).sort( {itemDate: -1 })   
+        }else if(req.session.sessionLocation == ""){
+            var lostItems = await lostItemsModel.find({Category : req.session.sessionCategory}).sort( {itemDate: -1 })   
+        }else{
+            var lostItems = await lostItemsModel.find({Category : req.session.sessionCategory, Location : req.session.sessionLocation}).sort( {itemDate: -1 })   
+        }
+        
         const number = lostItems.length;
         console.log("number of items: ",  number)
-        res.render("filter_page",{items:{item:lostItems, activeAdmin: req.session.activeAdmin, num:number}})
+        res.render("filter_page",{items:{item:lostItems, activeAdmin: req.session.activeAdmin, num:number , category : req.session.sessionCategory , location : req.session.sessionLocation}})
     }
     catch(e)
     {
